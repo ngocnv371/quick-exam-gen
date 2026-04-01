df65024461384f3bdbd6b4eea3038d3004356dcf
Ngoc Ngo <ngocnv371@gmail.com>
2026-04-01 10:29:40 +0700

    add sonner

---
+++ nextjs/components/ui/sonner.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import type { ComponentProps } from "react";
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = ComponentProps<typeof Sonner>;
+
+export function Toaster(props: ToasterProps) {
+  return <Sonner closeButton richColors position="top-right" {...props} />;
+}
